117afe830614cfa7f29ad94ebb868f7b680dc712
Ilya Sychkov <Sychqh@gmail.com>
2023-01-20 19:27:26 +0500

    - some refactor

--- TelegramBot/Application/Commands/CommandLists.cs
+++ TelegramBot/Application/Commands/CommandLists.cs
@@ -7,7 +7,7 @@ public static class CommandLists
 {
     private static readonly IEnumerable<ICommand> AllCommands = InterfaceUtils.GetImplementedClasses<ICommand>();
 
-    public static IEnumerable<ICommand> StartCommands = AllCommands.Where(cmd => cmd is StartCommand or StartLoginCommand);
+    public static readonly IEnumerable<ICommand> StartCommands = AllCommands.Where(cmd => cmd is StartCommand or StartLoginCommand);
 
     public static readonly IEnumerable<ICommand> MainCommands = AllCommands.Where(cmd =>
         cmd is not StartLoginCommand && cmd is not StartCommand && cmd is not WrongCommand && cmd is not LoginCommand);
