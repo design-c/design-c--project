8d11ed8b182ed294c9de6b4c5b660a4d00c0ee31
Sergey Dorokhov <s.dorokhov@artsofte.ru>
2023-01-22 23:38:28 +0500

    unfix

--- TelegramBotApi/Api/DependencyRegistration/AddDomainDependencies.cs
+++ TelegramBotApi/Api/DependencyRegistration/AddDomainDependencies.cs
@@ -14,13 +14,8 @@ public static class AddDomainServices
             .AddTransient<IAuthService, AuthService>()
             .AddTransient<IUrfuUserServerDataService, UrfuUserServerDataService>()
             .AddTransient<IUserDataService, UrfuUserDataService>();
-        services.AddHttpClient<HttpClient>()
-            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
-            {
-                ClientCertificateOptions = ClientCertificateOption.Manual,
-                ServerCertificateCustomValidationCallback =
-                    (httpRequestMessage, cert, cetChain, policyErrors) => true
-            });
+
+        services.TryAddScoped<HttpClient>();
         services.TryAddScoped<WebClient>();
         services.TryAddScoped<HtmlParser>();
     }
